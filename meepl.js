a2154f99bef787cb12e943ca21eb8e7408ad5c2c
Anton Puhach <pugachag@gmail.com>
2020-07-06 21:15:19 +0200

    Update meepl.js

--- meepl.js
+++ meepl.js
@@ -1,17 +1,17 @@
+console.log("init");
 setTimeout(function() {
     FisionSDK.initialize({
-       apiKey: "de3282aa-137e-4220-83b0-675203f44614"
-       // ... other configuration
+        apiKey: "de3282aa-137e-4220-83b0-675203f44614"
     }).then(function(fisionSDK) {
-       fisionSDK.bodyProfileButton({
-        parentElement: document.getElementById('meepl_btn'),
-        onBodyProfileReceived: function (bodyFeatures) {
-           console.log('Received bodyfeatures:', bodyFeatures);
-           document.getElementById("meepl_res").innerHTML = JSON.stringify(bodyFeatures.measurements, null, 2);
-        },
-        onSignOut: function () {
-          console.log('The user has been signed out.');
-        }
-       });
+        fisionSDK.bodyProfileButton({
+            parentElement: document.getElementById('meepl_btn'),
+            onBodyProfileReceived: function (bodyFeatures) {
+                console.log('Received bodyfeatures:', bodyFeatures);
+                document.getElementById("meepl_res").innerHTML = JSON.stringify(bodyFeatures.measurements, null, 2);
+            },
+            onSignOut: function () {
+                console.log('The user has been signed out.');
+            }
+        });
     });
-  }, 2000);
+}, 2000);
